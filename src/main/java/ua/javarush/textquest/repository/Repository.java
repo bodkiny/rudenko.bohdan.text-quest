7f92e6e054fee21a161646c8e9468c425c70d179
Bohdan Rudenko <brudenko2804@gmail.com>
2024-01-31 19:08:42 +0200

    Add the clarification to the javadocs of create method

--- src/main/java/ua/javarush/textquest/repository/Repository.java
+++ src/main/java/ua/javarush/textquest/repository/Repository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 public interface Repository<T> {
     /**
      * Create entity in database
+     * Does not check if entity already exists
      *
      * @param entity entity
      * @return created entity
